9fdaa0a23e7390056e7eab8a75fe52e328ce7785
Ivanets <evgeniyivanets@gmail.com>
2020-05-18 13:38:11 +0300

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,8 +8,8 @@ fi
 domains=(stage.click-knock.com www.stage.click-knock.com wsl.stage.click-knock.com preview.stage.click-knock.com product.stage.click-knock.com file.stage.click-knock.com)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="aka@ivanets.online" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
